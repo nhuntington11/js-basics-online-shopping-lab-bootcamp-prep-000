d9b6dff588e8bcb231ebe93b946bac49a76e03f9
Nick H <nhuntington11@gmail.com>
2018-06-21 02:28:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -48,7 +48,7 @@ function total() {
 function removeFromCart(item) {
   for (var i = 0; cart[i] != undefined; i++) {
     if (item === cart[i].itemName) {
-      cart[i].itemName.delete
+      delete cart[i]
     }
   }
 }
